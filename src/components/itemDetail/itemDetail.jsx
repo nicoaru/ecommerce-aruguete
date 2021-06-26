acf2a09171cd6d271df22e5e3579b761cb5bd1d8
Nico Aru <nico_aru@yahoo.com.ar>
2021-06-26 16:30:58 -0300

    clase7 - cree itemDetailContainer e itemDetail y le agregue el spinner de carga

--- src/components/itemDetail/itemDetail.jsx
+++ src/components/itemDetail/itemDetail.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import './itemDetail.css'
+import loadingGif from '../../recursos/spinner.svg'
 
 function ItemDetail({item}) {
 
 
     return (
-        <div id={item.id} className='card'>
-            <div className='imgContainer'>
+        <div>
+            { item.length === 0 ?
+            <img src={loadingGif} alt='spinner' height='150px'/> 
+            :
+            <div id={item.id} className='card'>
+                <div className='imgContainer'>
                 <img src={item.pictureUrl} width='100%' alt='imagen del producto'/>
-            </div>            
-            <h1 className='className'>{item.title}</h1>
-            <h2 className='price'>$ {item.price}</h2>
-            <p className='description'>{item.description}</p>
-
+                </div>            
+                <h1 className='className'>{item.title}</h1>
+                <h2 className='price'>$ {item.price}</h2>
+                <p className='description'>{item.description}</p>
+            </div>   
+            }
+            
         </div>
+
     )
 }
 
